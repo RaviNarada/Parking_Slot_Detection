0c5d73a2f600f569aa98c7d51c944e80f45ab728
RaviNarada <ravikumarnarada@gmail.com>
2025-07-20 20:18:52 +0530

    Updated Application.py

--- application.py
+++ application.py
@@ -288,5 +288,6 @@ async def get_system_status():
     }
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=5000)
+   # uvicorn.run(app, host="127.0.0.1", port=5000) - This is for Windows
+    uvicorn.run(app, host="0.0.0.0", port=10000)
 
